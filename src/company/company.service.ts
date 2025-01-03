1ee31f46a56a98104fc94e64142c202705d9aafc
Juan Martin Di Martino <juan.dimartino@intive.com>
2025-01-03 00:37:44 -0300

    emprolijo servicio

--- src/company/company.service.ts
+++ src/company/company.service.ts
@@ -8,6 +8,11 @@ import { FindQueryOptions } from '../constants/common';
 
 @Injectable()
 export class CompanyService {
+  private readonly methods: any = {
+    findAll: this.buildOptionsForFindAll,
+    findAllByTransactions: this.buildOptionsForFindAllByTransactions,
+    findById: this.buildOptionsForFindById
+  };
   constructor(private utils: UtilsService, private dataSource: DataSource, private mapper: CompanyMapper) {}
   async create(dto: CompanyDTO): Promise<any> {
     const queryRunner = this.dataSource.createQueryRunner();
@@ -26,39 +31,55 @@ export class CompanyService {
 
   async findAll(args: FindQueryOptions = {}): Promise<Array<CompanyDTO>> {
     const queryRunner = this.dataSource.createQueryRunner();
-    const options: any = this.buildOptions(args);
-    const today: Date = new Date();
-    options.where = {
-      createDate: Between(args.where?.createDateSince || today.setDate(today.getDate() - 30), args.where?.createDateTo || today)
-    };
+    const options: any = this.buildOptions(args, 'findAll');
     return this.utils.buildDTO(await queryRunner.manager.find(Company, options), CompanyDTO);
   }
 
-  private buildOptions(options: FindQueryOptions): any {
-    const result: any = {};
+  private buildOptions(options: FindQueryOptions, methodName: string): any {
+    let result: any = {};
+    let aux: any = {};
     if (options.skip) result.skip = options.skip;
     if (options.take) result.take = options.take;
+    if (!options.where || !Object.keys(options.where).length) return result;
+    if (this.methods[methodName]) aux = this.methods[methodName](options);
+    result = { ...result, ...aux };
     return result;
   }
 
   async findById(id: number): Promise<CompanyDTO> {
     const queryRunner = this.dataSource.createQueryRunner();
-    let options: any = {};
-    const relations: string[] = ['transactions', 'transactions.accountTarget', 'transactions.accountSource', 'transactions.accountTarget.accountType', 'transactions.accountSource.accountType'];
-    options = { where: { id }, relations };
+    const options: any = this.buildOptions({ where: { id } }, 'findById');
     return this.utils.buildDTO(await queryRunner.manager.findOne(Company, options), CompanyDTO);
+  }
+
+  private buildOptionsForFindAll(args: any): any {
+    const today: Date = new Date();
+    return {
+      where: {
+        createDate: Between(args.where?.createDateSince || today.setDate(today.getDate() - 30), args.where?.createDateTo || today)
+      }
+    };
+  }
+
+  private buildOptionsForFindAllByTransactions(args: any): any {
+    const today: Date = new Date();
+    return {
+      where: {
+        transactions: {
+          transferDate: Between(args.where?.transferDateSince || today.setDate(today.getDate() - 30), args.where?.transferDateTo || today)
+        }
+      },
+      relations: ['transactions']
+    };
+  }
+
+  private buildOptionsForFindById(args: any): any {
+    return { where: { id: args.where.id }, relations: ['transactions', 'transactions.accountTarget', 'transactions.accountSource', 'transactions.accountTarget.accountType', 'transactions.accountSource.accountType'] };
   }
 
   async findAllByTransactions(args: FindQueryOptions = {}): Promise<Array<CompanyDTO>> {
     const queryRunner = this.dataSource.createQueryRunner();
-    const options: any = this.buildOptions(args);
-    const today: Date = new Date();
-    options.where = {
-      transactions: {
-        transferDate: Between(args.where?.transferDateSince || today.setDate(today.getDate() - 30), args.where?.transferDateTo || today)
-      }
-    };
-    options.relations = ['transactions'];
+    const options: any = this.buildOptions(args, 'findAllByTransactions');
     return this.utils.buildDTO(await queryRunner.manager.find(Company, options), CompanyDTO);
   }
 }
